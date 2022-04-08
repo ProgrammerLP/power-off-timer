07b93a35a3367cba40453f73ecf86ef0bd398a66
ProgrammerLP <101939228+ProgrammerLP@users.noreply.github.com>
2022-04-08 19:45:54 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace PowerOff_Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_timer = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.TB_h = new System.Windows.Forms.TextBox();
+            this.TB_min = new System.Windows.Forms.TextBox();
+            this.TB_sec = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.RB_sp = new System.Windows.Forms.RadioButton();
+            this.RB_rs = new System.Windows.Forms.RadioButton();
+            this.RB_sd = new System.Windows.Forms.RadioButton();
+            this.t1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbl_timer);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(206, 128);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Timer";
+            // 
+            // lbl_timer
+            // 
+            this.lbl_timer.AutoSize = true;
+            this.lbl_timer.Location = new System.Drawing.Point(7, 53);
+            this.lbl_timer.Name = "lbl_timer";
+            this.lbl_timer.Size = new System.Drawing.Size(49, 13);
+            this.lbl_timer.TabIndex = 0;
+            this.lbl_timer.Text = "00:00:00";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(22, 156);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 48);
+            this.btn_start.TabIndex = 1;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(134, 156);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 48);
+            this.btn_stop.TabIndex = 2;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.TB_h);
+            this.groupBox2.Controls.Add(this.TB_min);
+            this.groupBox2.Controls.Add(this.TB_sec);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(235, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(190, 192);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Set Timer";
+            // 
+            // TB_h
+            // 
+            this.TB_h.Location = new System.Drawing.Point(79, 39);
+            this.TB_h.Name = "TB_h";
+            this.TB_h.Size = new System.Drawing.Size(100, 20);
+            this.TB_h.TabIndex = 5;
+            // 
+            // TB_min
+            // 
+            this.TB_min.Location = new System.Drawing.Point(79, 88);
+            this.TB_min.Name = "TB_min";
+            this.TB_min.Size = new System.Drawing.Size(100, 20);
+            this.TB_min.TabIndex = 4;
+            // 
+            // TB_sec
+            // 
+            this.TB_sec.Location = new System.Drawing.Point(79, 141);
+            this.TB_sec.Name = "TB_sec";
+            this.TB_sec.Size = new System.Drawing.Size(100, 20);
+            this.TB_sec.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Seconds";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Minutes";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hours";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.RB_sp);
+            this.groupBox3.Controls.Add(this.RB_rs);
+            this.groupBox3.Controls.Add(this.RB_sd);
+            this.groupBox3.Location = new System.Drawing.Point(443, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(221, 192);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Action";
+            // 
+            // RB_sp
+            // 
+            this.RB_sp.AutoSize = true;
+            this.RB_sp.Location = new System.Drawing.Point(25, 144);
+            this.RB_sp.Name = "RB_sp";
+            this.RB_sp.Size = new System.Drawing.Size(83, 17);
+            this.RB_sp.TabIndex = 2;
+            this.RB_sp.Text = "Save Power";
+            this.RB_sp.UseVisualStyleBackColor = true;
+            // 
+            // RB_rs
+            // 
+            this.RB_rs.AutoSize = true;
+            this.RB_rs.Location = new System.Drawing.Point(25, 91);
+            this.RB_rs.Name = "RB_rs";
+            this.RB_rs.Size = new System.Drawing.Size(59, 17);
+            this.RB_rs.TabIndex = 1;
+            this.RB_rs.Text = "Restart";
+            this.RB_rs.UseVisualStyleBackColor = true;
+            // 
+            // RB_sd
+            // 
+            this.RB_sd.AutoSize = true;
+            this.RB_sd.Checked = true;
+            this.RB_sd.Location = new System.Drawing.Point(25, 42);
+            this.RB_sd.Name = "RB_sd";
+            this.RB_sd.Size = new System.Drawing.Size(73, 17);
+            this.RB_sd.TabIndex = 0;
+            this.RB_sd.TabStop = true;
+            this.RB_sd.Text = "Shutdown";
+            this.RB_sd.UseVisualStyleBackColor = true;
+            // 
+            // t1
+            // 
+            this.t1.Interval = 1000;
+            this.t1.Tick += new System.EventHandler(this.t1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 229);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Shutdown Timer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl_timer;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox TB_h;
+        private System.Windows.Forms.TextBox TB_min;
+        private System.Windows.Forms.TextBox TB_sec;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton RB_sp;
+        private System.Windows.Forms.RadioButton RB_rs;
+        private System.Windows.Forms.RadioButton RB_sd;
+        private System.Windows.Forms.Timer t1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerOff_Timer
+{
+    public partial class Form1 : Form
+    {
+        TimeSpan timeleft;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            CheckTextBoxValues();
+
+            bool isStartable = true;
+
+            try
+            {
+                timeleft = new TimeSpan(int.Parse(TB_h.Text), int.Parse(TB_min.Text), int.Parse(TB_sec.Text));
+            }
+            catch (Exception ex)
+            {
+                isStartable = false;
+                MessageBox.Show(ex.Message);
+            }
+
+            if (isStartable)
+            {
+                t1.Start();
+                lbl_timer.Text = timeleft.ToString(@"hh\:mm\:ss");
+            }
+
+        }
+
+        private void t1_Tick(object sender, EventArgs e)
+        {
+            timeleft = timeleft.Subtract(TimeSpan.FromSeconds(1));
+            try
+            {
+                lbl_timer.Text = timeleft.ToString(@"hh\:mm\:ss");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (timeleft.TotalSeconds <= 0)
+            {
+                t1.Stop();
+                PerformAction();
+            }
+        }
+
+        private void PerformAction()
+        {
+            if (RB_sd.Checked)
+            {
+                Process.Start("shutdown", "/s");
+            }
+            else if (RB_rs.Checked)
+            {
+                Process.Start("shutdown", "/r");
+            }
+            else if (RB_sp.Checked)
+            {
+                Process.Start("rundll32.exe", "powrprof.dll,SetSuspendState");
+            }
+        }
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            t1.Stop();
+            lbl_timer.Text = "00:00:00";
+        }
+
+        private void CheckTextBoxValues()
+        {
+            if (TB_h.Text.Count() == 0)
+            {
+                TB_h.Text = "0";
+            }
+
+            if (TB_min.Text.Count() == 0)
+            {
+                TB_min.Text = "0";
+            }
+
+            if (TB_sec.Text.Count() == 0)
+            {
+                TB_sec.Text = "0";
+            }
+        }
+
+    }
+}
